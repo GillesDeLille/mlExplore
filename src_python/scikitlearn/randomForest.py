b20cbaf28d3776cff95c0df505a9ba0696e56a54
GillesDeLille <gilles.teixidor@laposte.net>
2020-07-31 10:14:38 +0200

    ressort du preprocessing

--- src_python/scikitlearn/randomForest.py
+++ src_python/scikitlearn/randomForest.py
@@ -17,9 +17,9 @@ from sklearn.metrics import recall_score
 
 # fonction skl_fit : traitement avec la bibliotthèque scikitlearn
 def skl_fit(datas, cible='cible'):
-  target=datas[cible]
-  data=datas.drop(cible, axis=1)
-  X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=12)
+  # target=datas[cible]
+  # data=datas.drop(cible, axis=1)
+  # X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=12)
   # --------------------------------------------------------------------------------------------------------------------------
   clf=ensemble.RandomForestClassifier(n_jobs=-1, random_state=321)
   clf.fit(X_train, y_train)
