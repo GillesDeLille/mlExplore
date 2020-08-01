0dc153fe2054d6590c14a2401867e7b685c5f1f1
GillesDeLille <gilles.teixidor@laposte.net>
2020-08-01 11:34:41 +0200

    renommage, orthographe

--- src_R/R/ranger.R
+++ src_R/R/ranger.R
@@ -1,5 +1,5 @@
 require(ranger)
-mdl <- ranger(as.formula(paste(target,'~ .')), data=features_target)
+mdl <- ranger(as.formula(paste(target,'~ .')), data=data)
 # //
 # TO DO
 # "ranger" un maximum de résultats sous la même forme que pour la sortie de skl (avez-vous apprécié le jeu de mot ?)
