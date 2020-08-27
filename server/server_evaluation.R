cfa0f14d5f5374e3a07853779fe65ec92be15e6f
GillesDeLille <gilles.teixidor@laposte.net>
2020-08-27 18:42:54 +0200

    eval2

--- server/server_evaluation.R
+++ server/server_evaluation.R
@@ -11,32 +11,60 @@ affichage_erreurEvaluation <- reactive({
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-output$editEvaluation <- renderUI({
-  input$annulerProcedure_evaluation
-
-  validerProcedure_evaluation=NULL ; if(!is.null(isolate(input$activerProcedure_evaluation))){ validerProcedure_evaluation=isolate(input$activerProcedure_evaluation) }
-  # activer=FALSE ; if(!is.null(validerProcedure_evaluation)) activer=validerProcedure_evaluation
-  
-  ed <- editeur('Procedure_evaluation', 'python', 30, activer=isolate(input$activerProcedure_evaluation), initScript=(is.null(validerProcedure_evaluation)))
+output$uiEvaluation <- renderUI({
+  liste <- list(
+    uiOutput('editEvaluation'),
+    column(12,hr()),
+    uiOutput('uiResultatsEvaluation')
+  )
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-output$uiEvaluation <- renderUI({
-  liste <- list(
-    column(10, uiOutput('editEvaluation')),
-    column(12,uiOutput('uiResultatsEvaluation'))
-  )
+output$editEvaluation <- renderUI({
+  input$annulerProcedure_evaluation
+  
+  validerProcedure_evaluation=NULL ; if(!is.null(isolate(input$activerProcedure_evaluation))){ validerProcedure_evaluation=isolate(input$activerProcedure_evaluation) }
+  # activer=FALSE ; if(!is.null(validerProcedure_evaluation)) activer=validerProcedure_evaluation
+  
+  ed <- editeur('Procedure_evaluation', 'python', 25, activer=isolate(input$activerProcedure_evaluation), initScript=(is.null(validerProcedure_evaluation)))
+})
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+output$uiResultatsEvaluation <- renderUI({
+  
+  liste <- NULL ; if(!is.null(input$activerProcedure_evaluation)) if(input$activerProcedure_evaluation){
+    if(!is.null(resultatsEvaluation())){
+      liste <- list(
+        box(
+          width = 3, title = 'Matrice de confusion',
+          DT::dataTableOutput('matconf')
+        ),
+        box(
+          width = 9,
+          verbatimTextOutput('rapport')
+        ),
+        # box(
+        #   width = 12,
+          # 'Learning Curve',
+          plotOutput('image_learningCurve')
+        # )
+      )
+    }else liste <- list( affichage_erreurEvaluation() )
+  }
+  liste
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
 resultatsEvaluation <- reactive({
   
-  input$activerProcedure_evaluation
+  validate( need(input$activerProcedure_evaluation,'...') )
+  
+  dossier=paste0('src/',applisession())
   
   print('=============================================================================================')
   print("=========================   calcul des resultats de l'évaluation  ===========================")
   
-  fichiersource=paste0('src/',applisession(),'/procedure_evaluation.py')
+  fichiersource=paste0(dossier,'/procedure_evaluation.py')
   print(fichiersource)
 
   # ----------------------------------  
@@ -56,53 +84,45 @@ resultatsEvaluation <- reactive({
   X_test <- data_preprocessed()$X_test
   y_test <- data_preprocessed()$y_test
   res <- tryCatch(
-    evaluation(RandomForest, X_train, y_train, X_test, y_test),
+    evaluation(RandomForest, X_train, y_train, X_test, y_test, dossier),
     error = function(e){e ; erreurEvaluation(e) }
   )
   if(!is.null(isolate(erreurEvaluation()))) print(paste('  sortie en erreur :',isolate(erreurEvaluation())))
   if(!is.null(isolate(erreurEvaluation()))) return(NULL)
 
-  print('=== Evaluation ===')
-  res1=res[[1]]
-  save(res1, file='~/resultats_eval.rdata')
-  
-  print(res1)
   print('=============================================================================================')
-  res
+
+  matrice_confusion=res[[1]]
+  classification_report=res[[2]]
+  fichier_report=paste0(dossier,'/classification_report.txt')
+  writeLines(classification_report, fichier_report)
+  
+  list(matrice_confusion=matrice_confusion, rapport=classification_report)
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-output$uiResultatsEvaluation <- renderUI({
-  
-  liste <- NULL
-  
-  if(!is.null(input$activerProcedure_evaluation)) if(input$activerProcedure_evaluation){
-    res <- resultatsEvaluation()
-    if(!is.null(res)){
-      liste <- list(
-        HTML('toto')
-      )
-    }else{
-      liste <- list(
-        affichage_erreurEvaluation()
-      )
-    }
-  }
-  liste
+output$matconf <- DT::renderDataTable({
+  res <- resultatsEvaluation()
+  matconf <- res$matrice_confusion %>% as_tibble()
+  matconf %>%
+    datatable(rownames = F, options = list(searching=F, paging=F, info=F))
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-courbeGain <- reactive({
-  listeModeles=c('randomForest')
-  courbeOk <- ((input$implementation=='scikitlearn/randomForest')&(input$modele %in% listeModeles))
+output$rapport <- renderText({
+  res <- resultatsEvaluation()
+  texte <- res$rapport
+  texte
 })
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------
-output$imageGain <- renderImage({
-  validate( need(courbeGain(),'Courbe de gain cumulée à construire selon contexte') )
+output$image_learningCurve <- renderImage({
+  validate( need(input$activerProcedure_evaluation,'...') )
   
-  filename <- normalizePath(file.path('./figures', 'courbeGainCumulée.png'))
-  list(src = filename, alt = 'Courbe de gain cumulée', height='250px')
+  dossier=paste0('src/',applisession())
+  
+  filename <- normalizePath(file.path(dossier, 'eval_learning_curve.png'))
+  list(src = filename, alt = 'Learning curve', width='100%')
 }, deleteFile = FALSE)
 
 
@@ -126,6 +146,29 @@ observe({
   input$editProcedure_evaluation
   updateCheckboxInput(session, 'activerProcedure_evaluation',  value = F)
 })
+
+
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+courbeGain <- reactive({
+  listeModeles=c('randomForest')
+  courbeOk <- ((input$implementation=='scikitlearn/randomForest')&(input$modele %in% listeModeles))
+})
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+output$imageGain <- renderImage({
+  validate( need(courbeGain(),'Courbe de gain cumulée à construire selon contexte') )
+  
+  filename <- normalizePath(file.path('./figures', 'courbeGainCumulée.png'))
+  list(src = filename, alt = 'Courbe de gain cumulée', height='250px')
+}, deleteFile = FALSE)
+
+
+
 
 
 
