f146dc49375c7730d65bbf1e3a25067fb523e3a8
GillesDeLille <gilles.teixidor@laposte.net>
2020-08-27 18:48:36 +0200

    résultats dans "dossier" Rien d'autre que RandomForest

--- src_python/procedure_evaluation.py
+++ src_python/procedure_evaluation.py
@@ -1,4 +1,12 @@
 
+# -->      Ce script doit définir une fonction qui ajuste un modele donné
+# -->      et renvoie des éléments d'évaluation.
+# -->      Les résultats sont stockés dans dans un "dossier"
+
+# -->      Elle sera appelée par mlExplore de la façon suivante :
+
+# -->      evaluation(model, X_train, y_train, X_test, y_test, dossier)
+
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,7 +16,7 @@ import seaborn as sns
 from sklearn.metrics import f1_score, confusion_matrix, classification_report
 from sklearn.model_selection import learning_curve
 
-def evaluation(model, X_train, y_train, X_test, y_test):
+def evaluation(model, X_train, y_train, X_test, y_test, dossier):
   model.fit(X_train, y_train)
   ypred = model.predict(X_test)
   print(confusion_matrix(y_test, ypred))
@@ -21,13 +29,16 @@ def evaluation(model, X_train, y_train, X_test, y_test):
   plt.figure(figsize=(12, 8))
   plt.plot(N, train_score.mean(axis=1), label='train score')
   plt.plot(N, val_score.mean(axis=1), label='validation score')
-  plt.legend()    
-  return (confusion_matrix(y_test, ypred), plt.show())
+  plt.legend()
+  plt.savefig(dossier + '/eval_learning_curve')
+  return (confusion_matrix(y_test, ypred), classification_report(y_test, ypred))
+  #return (confusion_matrix(y_test, ypred), classification_report(y_test, ypred), plt.show())
 
-from sklearn.tree import DecisionTreeClassifier
+
+# -------------------------------------------------------------------------------------------------------------
+
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
@@ -35,8 +46,4 @@ from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 preprocessor = make_pipeline(PolynomialFeatures(2, include_bias=False), SelectKBest(f_classif, k=10))
 
 RandomForest = make_pipeline(preprocessor, RandomForestClassifier(random_state=0))
-AdaBoost = make_pipeline(preprocessor, AdaBoostClassifier(random_state=0))
-SVM = make_pipeline(preprocessor, StandardScaler(), SVC(random_state=0))
-KNN = make_pipeline(preprocessor, StandardScaler(), KNeighborsClassifier())
 
-# evaluation(model)
