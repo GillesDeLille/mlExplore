272f5e6b5395f6475b9baaab97032623c1b420ea
GillesDeLille <gilles.teixidor@laposte.net>
2020-08-27 18:39:05 +0200

    valeur par défaut : RandomForest

--- server/server_description.R
+++ server/server_description.R
@@ -1,8 +1,17 @@
 
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+output$uiModeles <- renderUI({
+  impl=fread('implementations.csv')
+  liste_modeles=impl$modele %>% unique()
+  selectInput('modele','Modèles disponibles', choices = liste_modeles)
+})
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
 output$uiDescription <- renderUI({
+  modele='RandomForest' ; if(!is.null(input$modele)) modele=input$modele
   list(
     setShadow(class = 'box'),
-    column(2,br()), box(width=8, includeMarkdown(paste0('markdown/',input$modele,'.Rmd'))), column(2, br())
+    column(2,br()), box(width=8, includeMarkdown(paste0('markdown/',modele,'.Rmd'))), column(2, br())
     # ,column(6,uiOutput('uiModeleValidite'))
   )
 })
